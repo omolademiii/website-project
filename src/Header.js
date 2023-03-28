b61a4fd062658958f15f9e49557d238f6eb76f1c
omolade <tosin.ogunmola44@gmail.com>
2023-03-28 12:12:32 +0100

    website project

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <header>
+            <h1>Welcome to my website!</h1>
+            <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default Header
